08fbadb5215eef50c314d9eb67660cf02b57c0fc
nayelide <nayelide@250labs2.myguest.virtualbox.org>
2023-04-17 22:37:35 -0700

    updating

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import threading
 import pandas as pd
 import requests
 import plotly.express as px
+import json
 
 def generate_data() -> List[int]:
     """Generate some random data."""
@@ -81,47 +82,47 @@ def run(offload: Optional[str] = None) -> float:
         data2 = process2(data)
         
     elif offload == 'process1':
-        data1 = None
-        def offload_process1(data):
-            nonlocal data1
+    
+        def offload_process1(mydata):
+            nonlocal data1  
             # TODO: Send a POST request to the server with the input data
-            response1 = requests.post(f"{offload_url}/process1", json=mydata)
-            data1 = response.json()
-           # print(data1)
-           return data1
+            message1 = requests.post(f"{offload_url}/process1", json=mydata)
+            #print("status code: ", message1.status_code)
+            data1 = message1.json()
+            return data1
             
-        thread = threading.Thread(target=offload_process1, args=(data,))
+        thread = threading.Thread(target=offload_process1, args=(data,)) 
         thread.start()
         data2 = process2(data)
         thread.join(2)
         
         # Question 2: Why do we need to join the thread here?
-        
         # Question 3: Are the processing functions executing in parallel or just concurrently? What is the difference?
         #   See this article: https://oxylabs.io/blog/concurrency-vs-parallelism
         #   ChatGPT is also good at explaining the difference between parallel and concurrent execution!
         #   Make sure to cite any sources you use to answer this question.
     elif offload == 'process2':
-        # TODO: Implement this case   	
-		nonlocal data2
-		#Send a POST request to the server with the input data
-		message2 = requests.post(f"{offload_url}/process2", json=mydata)
-		data2 = message2.json()
-		return data2
+        # TODO: Implement this case
+        def offload_process2(mydata):
+        	nonlocal data2
+        	#Send a POST request to the server with the input data
+        	message2 = requests.post(f"{offload_url}/process2", json=mydata)
+        	data2 = message2.json()
+        	return data2
+        	
         thread = threading.Thread(target=offload_process2, args=(data,))
         thread.start()
         data1 = process1(data)
-        thread.join(2)	
-        #pass
+        thread.join(2)
+
     elif offload == 'both':
         # TODO: Implement this case
-          #Send a POST request to the server with the input data
+        #Send a POST request to the server with the input data
         message1 = requests.post(f"{offload_url}/process1", json=data)
         message2 = requests.post(f"{offload_url}/process2", json=data)
         data1 = message1.json()
         data2 = message2.json()
         
-       # pass
 
     ans = final_process(data1, data2)
     return ans 
@@ -153,14 +154,16 @@ def main():
 	
 	df = pd.DataFrame(data, columns = ['mode','time_mean','time_std'])
 	
-
+	
     # TODO: Plot makespans (total execution time) as a bar chart with error bars
     # Make sure to include a title and x and y labels
 	fig = px.bar (df, x = "mode", y = "time_mean", error_y = "time_std", labels = {"mode":"Offload Mode", "time_mean":"Time Mean (ms)"}, title= "Makespans (total execution time)")
+	#fig.show()
+
 
     # TODO: save plot to "makespan.png"
-
 	fig.write_image("makespan.png")
+
 
     # Question 4: What is the best offloading mode? Why do you think that is?
     # Question 5: What is the worst offloading mode? Why do you think that is?
